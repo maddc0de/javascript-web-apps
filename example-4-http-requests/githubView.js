a106fcf18715bdbd0557d93d7781ccf70e7ae3ab
Madelane <madelanedaz@gmail.com>
2023-05-04 20:14:05 +0100

    set contents of #repo-name & #repo-description equal to the values coming from the github repo data

---
+++ example-4-http-requests/githubView.js
@@ -0,0 +1,31 @@
+class GithubView {
+  constructor(model, client) {
+    this.model = model;
+    this.client = client;
+    this.repoInputEl = document.querySelector("#repo-name-input");
+
+    const submitButtonEl = document.querySelector("#submit-button");
+    submitButtonEl.addEventListener("click", this.addRepoInfo.bind(this));
+  }
+
+  display() {
+    const info = this.model.getRepoInfo();
+    // sets the content of #repo-name el to equal the value of the name property from repo data
+    const repoName = document.querySelector("#repo-name");
+    repoName.textContent = info.name;
+    // Set the content of the #repo-description el to equal the value of the description property from repo data.
+    const repoDesc = document.querySelector("#repo-description");
+    repoDesc.textContent = info.description;
+  }
+
+  addRepoInfo() {
+    const repoName = this.repoInputEl.value;
+    this.client.getRepoInfo(repoName, (repoInfo) => {
+      this.model.setRepoInfo(repoInfo);
+      // calling .display inside the callback function ensures that `repoInfo` has the data before trying to access .display method
+      this.display();
+    });
+  }
+}
+
+module.exports = GithubView;
